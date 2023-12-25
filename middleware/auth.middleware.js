f02a6357204fc2ebbe624b27eb673b540d78de86
Azizbek1 <azizochilov569@gmail.com>
2023-12-25 23:00:02 +0500

    server

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS') {
-       return next()
+        return next()
     }
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
-            return res.status(401).json({message: 'Ошибка аутентификации Нет Токена'})
+            return res.status(401).json({ message: 'Ошибка аутентификации Нет Токена' })
         }
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
         next()
     } catch (e) {
-        return res.status(401).json({message: 'Ошибка аутентификации'})
+        return res.status(401).json({ message: 'Ошибка аутентификации' })
     }
 }
